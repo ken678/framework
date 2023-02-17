7925de60147d726e25eb63fcaf59af8e425f441e
ken678 <530765310@qq.com>
2023-02-17 13:11:25 +0800

    改进 PHP 版本判断

--- library/think/View.php
+++ library/think/View.php
@@ -184,7 +184,7 @@ class View
 
         // 页面缓存
         ob_start();
-        if (PHP_VERSION > 8.0) {
+        if (version_compare(PHP_VERSION, '8.0', '>=')) {
             ob_implicit_flush(false);
         } else {
             ob_implicit_flush(0);
